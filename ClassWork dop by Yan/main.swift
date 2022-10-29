ba9b5788e58d3259157e29e7414e5257e2d98d14
Magdiel <magdiel.altynbekov@gmail.com>
2022-10-29 19:21:21 +0600

    вот

--- ClassWork dop by Yan/main.swift
+++ ClassWork dop by Yan/main.swift
@@ -7,5 +7,46 @@
 
 import Foundation
 
-print("Hello, World!")
 
+
+
+var s = [[1,2], ["carhartt","apple","nitro"], [4,4,5], [9,0]]
+
+
+
+
+for o in 0..<s.count where s[o].count >= 3{
+    print("Номер массива: \(o). Массив: \(s[o])")
+    for i in s[o]{
+        print("Элемент массива: \(i)")
+    }
+}
+
+s.forEach{
+    
+    if $0.count >= 3{
+        print($0)
+    }
+    for i in $0{
+        print(i)
+    }
+    
+}
+
+
+var answer = ""
+
+var array = [Int]()
+
+while answer != "no"{
+        answer = readLine()!
+        
+        if let number = Int(answer){
+    array.append(number)
+}
+}
+
+print(array)
+
+array = array.sorted()
+print(array)
